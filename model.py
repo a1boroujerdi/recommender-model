4694b186b9b88303437912b0e773bf0ac7ebac67
a1boroujerdi <91847536+a1boroujerdi@users.noreply.github.com>
2025-06-29 17:14:54 +0400

    Update model.py

--- model.py
+++ model.py
@@ -128,4 +128,4 @@ if __name__ == '__main__':
 
     print("Model loaded and trained.")
     print("Available products:", product_list)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80) 
